d1392ebcc50f6cb127c1e279b5c4c8394f8f36a9
HG-Laptop <1992hg@gmail.com>
2015-09-25 20:58:45 +0200

    Working line-printer for the camera

---
+++ dhackathon-sthlm-2015/Assets/Scripts/LinePrinter.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LinePrinter : MonoBehaviour
+{
+    public struct DrawObject
+    {
+        public DrawObject(Vector3[] l, Color c)
+        {
+            List = l;
+            Color = c;
+        }
+        public Vector3[] List;
+        public Color Color;
+    }
+
+    public Material m_material;
+    public Queue<DrawObject> Objects;
+    private Camera m_camera;
+
+    private void Awake()
+    {
+        Objects = new Queue<DrawObject>();
+    }
+
+    private void Start()
+    {
+        m_camera = gameObject.GetComponent<Camera>();
+    }
+
+    private void OnPostRender()
+    {
+        if (!m_material)
+        {
+            Debug.LogError("Please Assign a material on the inspector");
+            return;
+        }
+
+        // Push matrix and stuff, and draw whole queue.
+        GL.PushMatrix();
+        m_material.SetPass(0);
+        GL.LoadIdentity();
+        GL.MultMatrix(m_camera.worldToCameraMatrix);
+        DrawQueue();
+        GL.PopMatrix();
+    }
+
+    private void DrawQueue()
+    {
+        while (Objects.Count > 0)
+        {
+            var obj = Objects.Dequeue();
+            var points = obj.List;
+            var color = obj.Color;
+
+            for (int i = 0; i < points.Length - 1; ++i)
+            {
+                Line(points[i], points[i + 1], color);
+            }
+        }
+    }
+
+    private void Line(Vector3 point0, Vector3 point1, Color color)
+    {
+        GL.Begin(GL.LINES);
+        GL.Color(color);
+        GL.Vertex(point0);
+        GL.Vertex(point1);
+        GL.End();
+    }
+}
