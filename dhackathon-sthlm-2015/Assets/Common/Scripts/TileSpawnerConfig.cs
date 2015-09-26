51d726d94b59b8772b1adbf9478567cb71bf5060
Backman <Arrebarree@gmail.com>
2015-09-26 11:54:15 +0200

    Tile shiet

--- dhackathon-sthlm-2015/Assets/Common/Scripts/TileSpawnerConfig.cs
+++ dhackathon-sthlm-2015/Assets/Common/Scripts/TileSpawnerConfig.cs
@@ -6,5 +6,9 @@ public class TileSpawnerConfig : ScriptableObject
     public int Width;
     public int Height;
     public int TileSize;
+    public float TileTweenDuration;
+    public float TimeUntilTileTweening;
+    public Vector2 TileTweenWaitInterval;
+    public AnimationCurve TileTweeningCurve;
     public GameObject[] Tiles;
 }
--- dhackathon-sthlm-2015/Assets/Common/Scripts/VisualFeedback.cs
+++ dhackathon-sthlm-2015/Assets/Common/Scripts/VisualFeedback.cs
@@ -4,18 +4,24 @@ using System.Collections;
 [RequireComponent(typeof(Shape))]
 public class VisualFeedback : MonoBehaviour 
 {
+    [SerializeField]
+    private float _intensity = 1f;
+    [SerializeField]
+    private bool _randomness;
     MusicFeedback m_musicFeedback;
+    private string _id;
 
-	void Start () 
+    void Start () 
     {
         m_musicFeedback = GameObject.FindGameObjectWithTag("MusicFeedback").GetComponent<MusicFeedback>();
+        _id = System.Guid.NewGuid().ToString("N");
         if(m_musicFeedback != null)
-            m_musicFeedback.AddShape(GetComponent<Shape>());
+            m_musicFeedback.AddShape(GetComponent<Shape>(), _id, _intensity, _randomness);
 	}
 
     void OnDestroy()
     {
         if (m_musicFeedback != null)
-            m_musicFeedback.RemoveShape(GetComponent<Shape>());
+            m_musicFeedback.RemoveShape(_id);
     }
 }
